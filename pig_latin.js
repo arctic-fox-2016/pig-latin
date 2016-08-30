e1eb4612451dc254b0327918b7784d5302f75ffd
Aji Lantang Mardika & Fadhli Ramadhani Arislan <pair+ajilantang.fadhlirams@hacktiv8.com>
2016-08-30 18:27:03 +0700

    pig_latin

--- pig_latin.js
+++ pig_latin.js
@@ -1,4 +1,21 @@
 "use strict"
+var input = prompt ("isi dict");
+var vowel = ["a","i","u","e","o"];
+var word = [];
 
-//use readline to fix this challenge
-const readline = require('readline');
+
+function pigLatin(word){
+  if(vowel.indexOf(word[0]) == -1){
+   return word.substring(1) + word[0] + "ay.";
+   for ()
+  }else{
+    return word;
+  }
+}
+// function jadikanKalimat(word){
+//
+// }
+console.log(pigLatin ("kata"));
+console.log(pigLatin ("bacanya"));
+  //use readline to fix this challenge
+  // const readline = require('readline');
