d2b21da9f09036bd91a4fba48d83b1710b325a70
Imam & Ivan Gerard <pair+imamnugraha23.ivangerard@hacktiv8.com>
2016-08-30 16:37:53 +0700

    Pig Latin

--- pig_latin.js
+++ pig_latin.js
@@ -2,3 +2,40 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+
+
+function pigLatin(userinput){
+  var vowel = ["a","i","u","e","o"];
+  var pecahKata = userinput.split("");
+  var bodyVowel ="";
+  var bodykata ="";
+  for (var i=0;i<vowel.length;i++) {
+    if (pecahKata[0]== vowel[i]){
+      bodyVowel += userinput;
+
+    }else{
+      var kataPertama = pecahKata[0];
+      for (var i=1;i<pecahKata.length;i++){
+         bodykata += pecahKata[i];
+
+      }
+       var result = bodykata+kataPertama+"ay" ;
+       return result;
+    }
+
+  }
+}
+
+function pigLatinSentences(sentences){
+  let arrSentences = sentences.split(" ");
+  let arrResult = [];
+  for(let i=0; i < arrSentences.length; i++){
+    //console.log(arrSentences[i]);
+    arrResult.push(pigLatin(arrSentences[i]));
+
+  }
+  return arrResult.join(" ");
+
+}
+
+console.log(pigLatinSentences("ayam pergi ke ayam pasar"));
