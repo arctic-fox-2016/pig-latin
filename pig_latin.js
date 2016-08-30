36fe3b0215303dde64447f7ba44b081d58f3107a
Andrew Tandiawan & ismanuri <pair+atandiawan.ismanuri@hacktiv8.com>
2016-08-30 12:36:57 +0700

    Pig Latin

--- pig_latin.js
+++ pig_latin.js
@@ -2,3 +2,26 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+let result = ""
+let endWord = ""
+let word = ""
+let convert = function (word){
+
+    if (word[0] == "a" ||word[0] == "i" ||word[0] == "u" ||word[0] == "e" ||word[0] == "o"){
+      return word
+
+    } else {
+      while ((word.length >0) && (word[0] !== "a" && word[0] !== "i" && word[0] !== "u" && word[0] !== "e" && word[0] !== "o")){
+        console.log("...",word[0])
+        endWord = endWord.concat(word[0])
+        console.log(">",endWord)
+        word = word.substring(1,(word.length))
+        console.log(word)
+      }
+    endWord = endWord.concat("ay");
+    result = word.concat(endWord);
+    }
+  return result
+}
+
+console.log(convert("bhaju"))
