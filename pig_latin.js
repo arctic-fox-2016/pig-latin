3bab519c7a69a156498a0233492e7cf1828d67aa
Ahmad Amin & Tevin <pair+ahmad.tevinstein@hacktiv8.com>
2016-08-30 14:10:15 +0700

    pig latin

--- pig_latin.js
+++ pig_latin.js
@@ -1,4 +1,21 @@
 "use strict"
 
+function convert_single_word(word){
+  var word;
+  var final_word;
+  vowels = ['A','I','U','E','O'];
+
+  if word = str.startsWith(vowels[]){
+    final_word = word;
+  }
+  else {
+    final_word = word.substr(1) + word.slice(0,1) + 'ay';
+  }
+  return final_word;
+}
+
+function convert_complete_sentence(sentence){
+
+}
 //use readline to fix this challenge
 const readline = require('readline');
