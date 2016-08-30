d26d620e75b71b3fa92f00fe2938d82f0f6541be
Ahyana Rizky Pratama & Liliwibi <pair+ahyanarizky.lilianti@hacktiv8.com>
2016-08-30 11:55:41 +0700

    Iteration One : convert single word

--- pig_latin.js
+++ pig_latin.js
@@ -2,3 +2,30 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+
+function pig_latin_1word(inputText) {
+  var vowel = ["a", "i", "u", "e", "o"];
+  var firstLetter = inputText.slice(0,1);
+  var isVowel = false;
+  var i = 0;
+  while ((i < vowel.length) ) {
+    if (firstLetter == vowel[i]) {isVowel = true;}
+    i++;
+  }
+
+  if (isVowel == false) {
+    var hasil = inputText.slice(1,inputText.length) + firstLetter + "ay";
+  } else { hasil = inputText;
+  }
+  return hasil;
+}
+
+function pig_latin(inputText){
+  
+
+}
+
+
+console.log("hello : " + pig_latin_1word("hello"));
+console.log("ayo jalan jalan : " + pig_latin_1word("jalan"));
+console.log("ayo jalan jalan : " + pig_latin_1word("ayo"));
