2ab2cfd2a14f9fdca8218004a91e7a7c53dea813
name": null, &   "name": null <pair+elkana.sahbana@hacktiv8.com>
2016-08-30 11:56:27 +0700

    sahbana n elkana maked pig-latin

--- pig_latin.js
+++ pig_latin.js
@@ -2,3 +2,40 @@
 
 //use readline to fix this challenge
 const readline = require('readline');
+var vowels = ["a","i","u","e","o"];
+var sentence = prompt();
+function pigword(word){
+word = word.split("");
+if(vowels.indexOf(word[0]) >= 0){
+  word.push("-yay");
+} else{
+  var s = word[0];
+  word[word.length] = s;
+  word[0]="";
+  word.push("-ay")
+}
+return word.join("")
+}
+
+function pigsentence(sentences) {
+  words = sentences.split(" ")  
+  return words.map(pigword).join("")
+}
+// words = words.split(" ");
+// for(var word in words){
+// word = word.split("");
+// if(vowels.indexOf(word[0]) >= 0){
+//   word.push("-yay");
+//   word = word.join("")
+// } else{
+//   var s = word[0];
+//   word[word.length] = s;
+//   word[0]="";
+//   word.push("-ay")
+//   word = word.join("")
+// }
+// }
+// console.log(words.join(""));
+
+
+console.log(pigsentence(sentence));
