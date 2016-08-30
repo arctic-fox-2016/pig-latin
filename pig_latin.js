54394b7256a6a9105c739a6dc2a904aed62009cb
Ari Adiprana & Ryan Andhaka <pair+ariadiprana.ryanandhaka@hacktiv8.com>
2016-08-30 12:19:14 +0700

    piglatin_Itir2

--- pig_latin.js
+++ pig_latin.js
@@ -1,30 +1,55 @@
 
-var depan = prompt("Please enter your word"); // abcxyzay.
+//var depan = prompt("Please enter your word");
+var depan = "";
 var belakang = "";
 var hasil = "";
 var vowel = "";
 var tampung = 0;
-    //prompt("Please enter your name");
-if (!
-   (depan.substring (0,1).includes("a") || depan.substring (0,1).includes("i") || depan.substring (0,1).includes("u") ||
-   depan.substring (0,1).includes("e") || depan.substring (0,1).includes("o"))
-  )
-{
-  //  console.log("masuk sini");
-  //  console.log(depan.indexOf("a"));
-  //  console.log(depan.substring(0,depan.indexOf("a")));
-  //  console.log(depan.substring(depan.indexOf("a"),depan.length))
-  console.log(depan.charAt(1).includes("i"));
-  vowel = cariVowel(depan);
-  console.log(vowel);
-  belakang = depan.substring(depan.indexOf(vowel),depan.length);
-  depan = depan.substring(0,depan.indexOf(vowel));
-  console.log("depan : "+ depan);
-  console.log("sisa : "+belakang);
-  hasil = belakang+depan+'ay.';
-  console.log ("hasil :" + hasil);
+var kalimat = prompt("Please enter your word");
+//
+// var str = "1,2,3,4,5,6";
+// var temp = new Array();
+// // this will return an array with strings "1", "2", etc.
+// temp = str.split(",");
 
+var temp = new Array();
+var temp2 = new Array();
+temp = kalimat.split(" ");
+console.log(temp)
+
+for (var i = 0; i < temp.length; i++) {
+  temp2 += pigLatin(temp[i]);
 }
+console.log("kalimat awal : " + kalimat);
+console.log("hasil        : " + temp2);
+console.log("jumlah kata yang di convert       : " + temp.length);
+
+
+
+function pigLatin(depan) {
+  if (!
+     (depan.substring (0,1).includes("a") || depan.substring (0,1).includes("i") || depan.substring (0,1).includes("u") ||
+     depan.substring (0,1).includes("e") || depan.substring (0,1).includes("o"))
+    )
+  {
+    //  console.log("masuk sini");
+    //  console.log(depan.indexOf("a"));
+    //  console.log(depan.substring(0,depan.indexOf("a")));
+    //  console.log(depan.substring(depan.indexOf("a"),depan.length))
+    // console.log(depan.charAt(1).includes("i"));
+    vowel = cariVowel(depan);
+    // console.log(vowel);
+    belakang = depan.substring(depan.indexOf(vowel),depan.length);
+    depan = depan.substring(0,depan.indexOf(vowel));
+    // console.log("depan : "+ depan);
+    // console.log("sisa : "+belakang);
+    depan = belakang+depan+'ay.';
+    // console.log ("hasil :" + kata);
+
+  }
+  return depan+' ';
+}
+
 
 function cariVowel(kata){
   for (var i = 0; i < kata.length; i++) {
